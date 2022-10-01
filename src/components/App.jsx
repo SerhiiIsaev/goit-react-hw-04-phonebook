4db8405873e7071eeb6fdbb7a195ca69845e93a8
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-10-01 16:01:23 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,67 @@
-import { Component } from "react";
 import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
 import { ContactForm } from "./ContactForm/ContactForm";
 import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
 
-export default class App extends Component {
+export const App = () => {
+  const [contacts, setContacts] = useState(()=> JSON.parse(localStorage.getItem('contactsList')) ?? [] );
+  const [filter, setFilter] = useState('');
+  const [filteredContacts, setFilteredContacts] = useState([])
 
-  state={
-    contacts: [],
-    filter: '',
+  const onFilterChange = (e) => {
+    setFilter(e.target.value)
   }
 
-  onFilterChange = (e) => {
-    this.setState({
-      filter: e.target.value
-    })
-  }
-
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state
+  const getFilteredContacts = () => {
     return contacts.filter(({name}) => name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
   }
 
-  addContact = (contact) => {
-    const {contacts} = this.state
-      const chekContact = contacts.filter(item => item.name.toLocaleLowerCase() === contact.name.toLocaleLowerCase())
-      if (chekContact.length > 0) {
-        return alert(`${contact.name} is already in contacts`)
-      } else {
-        this.setState((prev) => {
+  const addContact = (contact) => {
+    
+    const chekContact = contacts.filter(item => item.name.toLocaleLowerCase() === contact.name.toLocaleLowerCase())
+    if (chekContact.length > 0) {
+      return alert(`${contact.name} is already in contacts`)
+    } else {
         const newContact = {
-            id: nanoid(),
-          ...contact
+          id: nanoid(),
+        ...contact
         }
-        return {
-          contacts: [...prev.contacts, newContact]
-        }
-      })}
-  }
-
-  handleSubmit = (e) => {
-        e.preventDefault();
-        const contact = {
-            name: e.target.name.value,
-            number: e.target.number.value,
-        }
-        this.addContact(contact);
-        e.target.number.value = '';
-        e.target.name.value = '';
-  }
-
-  removeContact = (id) => {
-    this.setState((prev) => {
-        const newContacts = prev.contacts.filter((item) => item.id !== id);
-        return {
-          contacts: newContacts
-        }
-    })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      console.log(prevState.contacts)
-      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts))
+      return setContacts([...contacts, newContact]);
     }
   }
 
-  componentDidMount() {
-    const saveContacts = JSON.parse(localStorage.getItem('contactsList'))? JSON.parse(localStorage.getItem('contactsList')) : [] ;
-    this.setState({contacts: saveContacts})
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const contact = {
+        name: e.target.name.value,
+        number: e.target.number.value,
+    }
+    addContact(contact)
+    e.target.number.value = '';
+    e.target.name.value = '';
+  }
+ 
+  const removeContact = (id) => {
+    const newContacts = contacts.filter((item) => item.id !== id);
+    return setContacts([...newContacts])
   }
 
-  render() {
-    const data = this.getFilteredContacts()
-    return(
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm handleSubmit={this.handleSubmit} />
-        <h2>Contacts</h2>
-        <Filter onFilterChange={this.onFilterChange} filter = {this.state.filter} />
-        <ContactList items = {data} removeContact={this.removeContact} />
-      </div>
-    )
-  };
+  useEffect(() => {
+    
+    localStorage.setItem('contactsList', JSON.stringify(contacts))
+    
+  }, [contacts])
+  
+  const data = getFilteredContacts()
+  return(
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter onFilterChange={onFilterChange} filter = {filter} />
+      <ContactList items = {data} removeContact={removeContact} />
+    </div>
+  )
+  
 };
